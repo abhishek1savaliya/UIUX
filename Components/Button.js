46a2db89ad304fdeacea0776409d4f81b077944f
Yashesh133 <yashcreative133@gmail.com>
2023-12-03 17:43:43 +0530

    Complate website commit

---
+++ Components/Button.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+const Button = ({ type, title, icon, variant , full }) => {
+  return (
+    <div>
+      <button
+        type={type}
+        className={`flexCenter gap-3 rounded-full border ${variant} w-full ${full && 'w-full'}`}
+      >
+        {icon && <Image src={icon} alt={title} width={24} height={24} />}
+        <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
+      </button>
+    </div>
+  );
+};
+
+export default Button;
