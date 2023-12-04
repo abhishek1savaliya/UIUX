baa888f0f7371914d4886e1c1e4319595da673f8
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-12-05 00:11:32 +0530

    add cdn

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8cnYrX7kBjzl77lo5qBttFTwwnmoz9LAjJ/2pxEVGFc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"QTeX+QrTES8FshIYo4hQmsOMqE/PcotAmNmw+GI9O9E=\"}"
